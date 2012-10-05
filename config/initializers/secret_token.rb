cdd164e784ba0eb9a30da935e4a1291fcba969e9
Eric Wright <eric@svetlana.(none)>
2012-10-04 20:17:54 -0400

    Retreive secret cookie token from Heroku ENV vars.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,14 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ichiban::Application.config.secret_token = '824fd701c7401f91a819a02a7b74e99af999bded154e967889b65d2aacdc7abad3e97f5fc6c1746db89434658f0cdf34b392b620502b29f2fd2fa1b19eac05ce'
+
+
+SECRET_COOKIE_TOKEN = Rails.env.production? ? ENV['SECRET_COOKIE_TOKEN'] : "09ur02u30230f2083mcojosdcsdcojoijeoq3joijdaoijdq222dcedqdqd2qea32r3f02uf3023fu032uf03uf203fu"
+if SECRET_COOKIE_TOKEN.blank?
+  puts "Secret cookie token not set!"
+  puts "Run heroku config:add SECRET_COOKIE_TOKEN=YOUR_SECRET_KEY"
+  exit
+else
+  Ichiban::Application.config.secret_token = SECRET_COOKIE_TOKEN
+end
+
