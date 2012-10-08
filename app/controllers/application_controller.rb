10b0739aa7de7c799da305041b73a8e95a0820a5
Eric Wright <eric@svetlana.(none)>
2012-10-08 16:03:11 -0400

    Add directory listing to header. Disable 500 error catching.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :current_user
 
   unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, :with => :render_500
+    # rescue_from Exception, :with => :render_500
     rescue_from ActionController::RoutingError, :with => :render_404
     rescue_from ActionController::UnknownController, :with => :render_404
     rescue_from ActionController::UnknownAction, :with => :render_404
