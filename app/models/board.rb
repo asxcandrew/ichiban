88e35818012fd8602a0484a2b8290650306f1aed
Eric Wright <eric@svetlana.(none)>
2012-10-29 02:48:00 -0400

    Fix exploit where board names can cause conflicting URLs.

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,8 +6,14 @@ class Board < ActiveRecord::Base
                              too_long: "A board's name must not exceed 300 characters." }
   validates_presence_of :name, message: "A board must have a name."
 
+
+  validates(:directory, 
+            :format => { :with => /^[a-z0-9]+[-a-z0-9]*[a-z0-9]+$/i,
+            message: "A board's directory must be alphanumeric and at least 2 characters." })
   validates_presence_of :directory, message: "A board must have a directory."
-  validates_uniqueness_of :directory, message: "A board's directory must be unique."
+  validates_uniqueness_of(:directory,
+                          case_sensitive: false,
+                          message: "A board's directory must be unique.")
 
   has_many :posts, primary_key: 'directory', foreign_key: 'directory', :dependent => :destroy
   has_many :suspensions, primary_key: 'directory', foreign_key: 'directory', :dependent => :destroy
