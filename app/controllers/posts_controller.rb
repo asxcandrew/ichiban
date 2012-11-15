65142bfbe928cb57de329b46a88fb6be4c8fe50c
Eric Wright <dev@ericwright.me>
2012-11-15 18:30:11 -0500

    Refactor posts controller to use new authorization. WIP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,11 +15,6 @@ class PostsController < ApplicationController
         format.html
         format.json { render json: @post }
       end
-    else
-      respond_to do |format|
-        format.html { render 'errors/error_404' }
-        format.json { render json: { error: 404 }.to_json, :status => :unprocessable_entity }
-      end
     end
   end
 
@@ -28,33 +23,20 @@ class PostsController < ApplicationController
     # Simulate different IP addresses
     @post.ip_address = @post.board.save_IPs ? request.ip : Array.new(4){rand(256)}.join('.')
     
-    path_options = {}
-
     # Only a bot would see this field.
     if !params[:email].blank?
       logger.info "Spam Bot detected"
       redirect_to request.referrer
     else
-      if @post.save
-        cookies.signed[:passphrase] = { value: params[:post][:tripcode], expires: 1.week.from_now }
-        cookies.signed[:name] = { value: @post.name, expires: 1.week.from_now }
-        cookies.signed[:tripcode] = { value: @post.tripcode, expires: 1.week.from_now }
+      @post.save!
+      cookies.signed[:passphrase] = { value: params[:post][:tripcode], expires: 1.week.from_now }
+      cookies.signed[:name] = { value: @post.name, expires: 1.week.from_now }
+      cookies.signed[:tripcode] = { value: @post.tripcode, expires: 1.week.from_now }
 
-        # Used to delete posts.
-        cookies.signed[@post.to_sha2] = { value: @post.ip_address, expires: 1.week.from_now }
+      # Used to delete posts.
+      cookies.signed[@post.to_sha2] = { value: @post.ip_address, expires: 1.week.from_now }
 
-        # flash[:notice] = I18n.t('posts.create.created', post_id: @post.id)
-
-        if @post.parent
-          redirect_to(request.referrer + "##{@post.id}")
-        else
-          redirect_to post_path(@post)
-        end
-
-      else
-        flash[:error] = @post.errors.first[1]
-        redirect_to request.referrer
-      end
+      redirect_to(@post.parent ? (request.referrer + "##{@post.id}") : post_path(@post))
     end
   end
 
@@ -64,14 +46,12 @@ class PostsController < ApplicationController
     # No sense in keeping them on a page without a parent.
     response[:redirect] = board_path(@post.board) if params[:redirect] == true.to_s
 
-    if cookies.signed[@post.to_sha2] == @post.ip_address || check_if_user_can!(:destroy, Post, @post)
-      if @post.destroy
-        response[:flash] = { :type => :notice, message: I18n.t('posts.destroy.deleted', post_id: @post.id) }
+    cookies.signed[@post.to_sha2] == @post.ip_address || check_if_user_can!(:destroy, Post, @post)
+    @post.destroy
+    response[:flash] = { :type => :notice, message: I18n.t('posts.destroy.deleted', post_id: @post.id) }
 
-        # Better clean up after ourselves.
-        cookies.delete(@post.to_sha2)
-      end
-    end
+    # Better clean up after ourselves.
+    cookies.delete(@post.to_sha2)
 
     render json: response
   end
