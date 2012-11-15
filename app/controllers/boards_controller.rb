7238ec72e34d6c6428fe3ad81a9c2113c727970c
Eric Wright <dev@ericwright.me>
2012-11-15 18:29:24 -0500

    Refactor board controller to use new authorization.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,6 @@
 class BoardsController < ApplicationController
   before_filter :find_boards, except: [:destroy]
-  before_filter :set_board, only: [:edit, :update, :show]
+  before_filter :set_board, except: [:index, :new, :create]
   load_and_authorize_resource
   
   def index
@@ -13,14 +13,8 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @board = Board.new(params[:board])
-
-    if @board.save
-      redirect_to boards_path, notice: "/#{@board.directory}/ created!"
-    else
-      flash[:error] = @board.errors.first[1]
-      render action: 'new'
-    end
+    @board = Board.create!(params[:board])
+    redirect_to boards_path, notice: "/#{@board.directory}/ created!"
   end
 
   def show
@@ -28,58 +22,35 @@ class BoardsController < ApplicationController
     @reply = Post.new
     @previews = 2
     @child_limit = 2
-    if @board
-      @posts = Post.threads_for(@board).order("updated_at DESC").page(params[:page])
+    @prefix = "#{@board.name}"
+    @posts = Post.threads_for(@board).order("updated_at DESC").page(params[:page])
 
-      @paged = params[:page] 
-      @prefix = "#{@board.name}"
+    @paged = params[:page] 
 
-      respond_to do |format|
-        format.html
-        format.json { render json: @posts }
-      end
-    else
-      respond_to do |format|
-        format.html { render 'errors/error_404' }
-        format.json { render json: { error: 404 }.to_json, :status => :unprocessable_entity }
-      end
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
     end
   end
 
   def destroy
-    @board = Board.find_by_id(params[:id])
-
-    if @board
-      if check_if_user?(can?(:manage, Board), @board)
-        if @board.destroy
-          redirect_to root_path, notice: I18n.t('boards.destroy.success', directory: @board.directory)
-        else
-          flash[:error] = @board.errors.first[1]
-          render action: 'edit'
-        end
-      else
-        raise CanCan::AccessDenied
-      end
-    else
-      redirect_to root_path, error: I18n.t('boards.errors.board_not_found', board_id: params[:id])
-    end
+    check_if_user_can!(:destroy, Board, @board)
+    @board.destroy
+    redirect_to root_path, notice: I18n.t('boards.destroy.success', directory: @board.directory)
   end
 
   def edit
-    raise CanCan::AccessDenied unless check_if_user?(can?(:update, Board), @board)
+    check_if_user_can!(:edit, Board, @board)
   end
 
+  # TODO: Fix issue where invalid records alter the edit view.
   def update
-    if check_if_user?(can?(:update, Board), @board)
-      if @board.update_attributes(params[:board])
-        redirect_to edit_board_path(@board), notice: I18n.t('boards.update.success')
-      else
-        flash[:error] = @board.errors.first[1]
-        render action: 'edit'
-      end
-    else
-      raise CanCan::AccessDenied
-    end
+    check_if_user_can!(:update, Board, @board)
+    
+    @edited_board = @board.clone
+    @edited_board.update_attributes!(params[:board])
+
+    redirect_to edit_board_path(@edited_board), notice: I18n.t('boards.update.success')
   end
 
   protected
@@ -88,7 +59,7 @@ class BoardsController < ApplicationController
     end
 
     def set_board
-      @board = Board.find_by_directory(params[:directory])
+      @board = Board.find_by_directory!(params[:directory])
     end
   # protected_end
 end
