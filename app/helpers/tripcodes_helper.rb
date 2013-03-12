3cc3b637d0790bd5d5c18bf893321cf1e0ae86d6
Eric Wright <dev@ericwright.me>
2013-03-12 00:45:27 -0400

    Clarify tripcode search functionality.

--- app/helpers/tripcodes_helper.rb
+++ app/helpers/tripcodes_helper.rb
@@ -2,6 +2,7 @@
 module TripcodesHelper
   def link_to_tripcode(tripcode, &block)
     path = url_for(controller: 'tripcodes', action: 'show', tripcode: tripcode)
-    link_to(path, title: I18n.t('posts.tripcodes.browse'), &block)
+    # link_to(path, title: I18n.t('posts.tripcodes.browse'), &block)
+    link_to('#', title: I18n.t('posts.tripcodes.browse'), &block)
   end
 end
