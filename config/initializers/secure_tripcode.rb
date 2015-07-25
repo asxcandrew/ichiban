20bc040f8deb4bb503b8b8285f393983f29d627a
asxcandrew <asxcandrew@gmail.com>
2015-07-25 23:06:18 +0300

    fixed secure tripcode init

--- config/initializers/secure_tripcode.rb
+++ config/initializers/secure_tripcode.rb
@@ -1,6 +1,6 @@
 # This is your secure tripcode salt. Changing the salt will alter future secure tripcodes!
 
-SECURE_TRIPCODE_SALT = Rails.env.production? ? Rails.application.secrets.some_api_key : "34290judcd0i03jc02d23rfa323dqc3era45zaa3fdae2vr6h65gwe4t2"
+SECURE_TRIPCODE_SALT = Rails.env.production? ? Rails.application.secrets.secure_tripcode_salt : "34290judcd0i03jc02d23rfa323dqc3era45zaa3fdae2vr6h65gwe4t2"
 
 if SECURE_TRIPCODE_SALT.nil? || SECURE_TRIPCODE_SALT.blank?
   puts "Secure tripcode seed not set!"
