ef283d4e2a5e47f351a9064bb0a8cf8c510f8180
asxcandrew <asxcandrew@gmail.com>
2015-07-25 23:11:43 +0300

    secret_cookie_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,12 +5,10 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-# SECRET_COOKIE_TOKEN = Rails.env.production? ? ENV['SECRET_COOKIE_TOKEN'] : "09ur02u30230f2083mcojosdcsdcojoijeoq3joijdaoijdq222dcedqdqd2qea32r3f02uf3023fu032uf03uf203fu"
-# if SECRET_COOKIE_TOKEN.nil? || SECRET_COOKIE_TOKEN.blank?
-#   puts "Secret cookie token not set!"
-#   puts "Run heroku config:add SECRET_COOKIE_TOKEN=YOUR_SECRET_KEY"
-#   exit
-# else
-#   Ichiban::Application.config.secret_token = SECRET_COOKIE_TOKEN
-# end
-
+SECRET_COOKIE_TOKEN = Rails.env.production? ? Rails.application.secrets.secret_cookie_token : "09ur02u30230f2083mcojosdcsdcojoijeoq3joijdaoijdq222dcedqdqd2qea32r3f02uf3023fu032uf03uf203fu"
+if SECRET_COOKIE_TOKEN.nil? || SECRET_COOKIE_TOKEN.blank?
+  puts "Secret cookie token not set!"
+  exit
+else
+  Ichiban::Application.config.secret_token = SECRET_COOKIE_TOKEN
+end
