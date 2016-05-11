e4c2f6de8c6440f654ac0cd9ad82e116b7742f90
nextanon <fakeanon.next@gmail.com>
2016-05-11 22:49:09 +0300

    Boards index view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,17 +1,17 @@
 class BoardsController < ApplicationController
   # before_filter :find_boards, except: [:destroy, :search]
   before_filter :set_board, except: [:index, :search]
-  
+
   def search
     @boards = params[:keyword] ? Board.where('name LIKE ?', "%#{params[:keyword]}%").limit(5) : []
     render json: @boards
   end
 
   def index
-    @posts = Post.all_threads.order("updated_at DESC").page(params[:page])
+    @boards = Board.order("updated_at DESC").page(params[:page])
     respond_to do |format|
-      format.html { render 'show' }
-      format.json { render json: @posts, except: [:ip_address], :include => :image }
+      format.html
+      format.json { render json: @boards }
     end
   end
 
@@ -19,7 +19,7 @@ class BoardsController < ApplicationController
     @prefix = "#{@board.name}"
     @posts = Post.threads_for(@board).order("updated_at DESC").page(params[:page])
 
-    @paged = params[:page] 
+    @paged = params[:page]
 
     respond_to do |format|
       format.html
